0d85c106f679cd4475161291de44e73e59d3b23c
Amo Ozmanyan <cool.ozmanyan2013@yandex.ru>
2023-05-24 23:23:39 +0300

    Initial Commit

---
+++ dreamneverdie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dreamneverdie
+//
+//  Created by Amahastla on 24.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
